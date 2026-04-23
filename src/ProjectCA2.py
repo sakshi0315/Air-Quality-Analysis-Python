a716432aceac035e81d82bf8ad5b8ecbba436684
Sakshi Kumari <sakshikumari0315@gmail.com>
2026-04-24 00:29:38 +0530

    Updated preprocessing code

--- src/ProjectCA2.py
+++ src/ProjectCA2.py
@@ -1,13 +1,15 @@
+
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 
+print("Hello")
 
 df = pd.read_csv("C:/Users/LENOVO/Downloads/AQI Dataset for project.csv")
 #pd.set_option('display.max_rows', 5000)
