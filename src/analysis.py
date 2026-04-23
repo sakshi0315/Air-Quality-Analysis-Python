8a1352f917fbf49e2330bd515fb398cdd35c9f3d
Sakshi Kumari <sakshikumari0315@gmail.com>
2026-04-24 00:56:03 +0530

    Added preprocessing steps

--- src/analysis.py
+++ src/analysis.py
@@ -10,3 +10,28 @@ from sklearn.metrics import mean_squared_error, r2_score
 
 df = pd.read_csv("C:/Users/LENOVO/OneDrive/Desktop/semester 4/Python Toolbox/Air-Quality-Analysis-Python/data/AQI Dataset for project.csv")
 #pd.set_option('display.max_rows', 5000)
+#pd.set_option('display.max_columns', 50)
+
+#print(df.head(100))
+print(df.describe())
+print(df.info())
+print(df.isna().sum()) 
+
+
+df = df.dropna()
+print(df.isna().sum())
+
+
+df['last_update'] = pd.to_datetime(df['last_update'], errors='coerce')
+print(df.info())
+df['city'] = df['city'].str.lower()
+df['state'] = df['state'].str.lower()
+
+
+df["Pollution Level"] = None
+df.loc[df["pollutant_avg"]<50, "Pollution Level"] = "Good"
+df.loc[((df["pollutant_avg"]>=50) & (df["pollutant_avg"]<100)), "Pollution Level"] = "Moderate"
+df.loc[df["pollutant_avg"]>=100, "Pollution Level"] = "Unhealthy"
+
+print(df.head(10))
+print(df.info())
